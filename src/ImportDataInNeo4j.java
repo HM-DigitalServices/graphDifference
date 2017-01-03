6d4f3fb04f8f3f46bebdeee5b9cebd52b2345728
Christian Möstl <Christian Mstl>
2017-01-03 14:02:57 +0100

    refactored + made compatible to Graph.java, ...

--- src/ImportDataInNeo4j.java
+++ src/ImportDataInNeo4j.java
@@ -20,8 +20,8 @@ public class ImportDataInNeo4j {
 
 	@SuppressWarnings("unused")
 	private final static String DELETE_ALL = "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r";
-	
-	private static Map<Integer, String> nodeLabels = new HashMap<Integer, String>();
+	private Map<Integer, String> nodeLabels = new HashMap<Integer, String>();
+	private static boolean[][] adjacency;
 
 	private enum Sports implements Label {
 		RUNNING, SWIMMING, BIKING;
@@ -30,40 +30,47 @@ public class ImportDataInNeo4j {
 	private enum RelationSport implements RelationshipType {
 		LOW, MIDDLE, HIGH, VERY_HIGH;
 	}
+	
+	public ImportDataInNeo4j() {
+	}
 
 	public static void main(String[] args) {
+		ImportDataInNeo4j obj = new ImportDataInNeo4j();
+		
 		GraphDatabaseBuilder d = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(new File(DB_PATH));
 		GraphDatabaseService x = d.newGraphDatabase();
 		
 		try (Transaction tx = x.beginTx()) {
-			Node a = x.createNode(Sports.RUNNING);
-			Node b = x.createNode(Sports.BIKING);
-			Node c = x.createNode(Sports.SWIMMING);
-			Node dd = x.createNode(Sports.RUNNING);
-
-			a.setProperty("Track", "one");
-			a.setProperty("Kind of", "Competition");
-			b.setProperty("Track", "two");
-			b.setProperty("Kind of", "training");
-			c.setProperty("Track", "four");
-			c.setProperty("Kind of", "training");
-			dd.setProperty("Track", "five");
-			dd.setProperty("Kind of", "intervall");
-
-			a.createRelationshipTo(b, RelationSport.MIDDLE);
-			b.createRelationshipTo(c, RelationSport.HIGH);
-			c.createRelationshipTo(a, RelationSport.VERY_HIGH);
-
-			boolean[][] adjacency = createAdjacency(x);
-
-			printAdjacency(adjacency);
+			obj.createGraph(x);
+			obj.createAdjacency(x);
+			obj.printAdjacency();
 
 			tx.success();
 		}
 	}
 
+	private void createGraph(GraphDatabaseService x) {
+		Node a = x.createNode(Sports.RUNNING);
+		Node b = x.createNode(Sports.BIKING);
+		Node c = x.createNode(Sports.SWIMMING);
+		Node dd = x.createNode(Sports.RUNNING);
+
+		a.setProperty("Track", "one");
+		a.setProperty("Kind of", "Competition");
+		b.setProperty("Track", "two");
+		b.setProperty("Kind of", "training");
+		c.setProperty("Track", "four");
+		c.setProperty("Kind of", "training");
+		dd.setProperty("Track", "five");
+		dd.setProperty("Kind of", "intervall");
+
+		a.createRelationshipTo(b, RelationSport.MIDDLE);
+		b.createRelationshipTo(c, RelationSport.HIGH);
+		c.createRelationshipTo(a, RelationSport.VERY_HIGH);
+	}
+
 	// Annahme n.getId() startet immer bei "index"=0
-	private static boolean[][] createAdjacency(GraphDatabaseService x) {
+	private void createAdjacency(GraphDatabaseService x) {
 		ArrayList<Node> nodes = new ArrayList<Node>();
 		ResourceIterator<Node> iter = x.getAllNodes().iterator();
 		while (iter.hasNext()) {
@@ -71,7 +78,7 @@ public class ImportDataInNeo4j {
 			nodes.add(n);
 			// System.out.println(n.getProperty("Track") + " " + n.getId());
 		}
-		boolean[][] adjacency = new boolean[nodes.size()][nodes.size()];
+		adjacency = new boolean[nodes.size()][nodes.size()];
 		for (int i = 0; i < nodes.size(); i++) {
 			Node n = nodes.get(i);
 			nodeLabels.put((int) n.getId(), (String) n.getProperty("Track"));
@@ -82,10 +89,13 @@ public class ImportDataInNeo4j {
 				adjacency[fst][snd] = true;
 			}
 		}
-		return adjacency;
+	}
+	
+	public Graph getAdjacency() {
+		return new Graph(adjacency, nodeLabels);
 	}
 
-	private static void printAdjacency(boolean[][] adjacency) {
+	private void printAdjacency() {
 		int fieldWidth = 10;
 		String formatS = "|%" + fieldWidth + "s";
 		String formatB = "|%" + fieldWidth + "b";
