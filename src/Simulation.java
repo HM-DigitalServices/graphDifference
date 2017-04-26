3e6e55b8cbb97a7786f1114d32c854122470e2c1
Floble <baer.florian@googlemail.com>
2017-04-26 13:30:28 +0200

    Hawaii

--- src/Simulation.java
+++ src/Simulation.java
@@ -7,15 +7,15 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class Simulation {
 	public static void main(String[] args) {
-		ImportDataInNeo4j obj = new ImportDataInNeo4j("/var/lib/neo4j/data/databases/graph.db");
+		ImportDataInNeo4j importer = new ImportDataInNeo4j("/var/lib/neo4j/data/databases/graph.db");
 
 		GraphDatabaseBuilder d = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(new File("/var/lib/neo4j/data/databases/graph.db"));
 		final GraphDatabaseService x = d.newGraphDatabase();
 
 		try (Transaction tx = x.beginTx()) {
-			obj.initGraph(x, obj);
+			importer.initGraph(x, importer);
 			System.out.println("Successful...");
-			obj.removeRandomNode(x, NodeLabel.SERVER);
+			importer.removeRandomNode(x, NodeLabel.SERVER);
 			tx.success();
 		}
 	}
