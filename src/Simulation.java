321d38719296a2c0ea07173acdd15f6f87a391b7
Christian Möstl <Christian Mstl>
2017-04-26 15:11:06 +0200

    modified pom.xml + added testmethod

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.util.Random;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
@@ -17,6 +19,22 @@ public class Simulation {
 			System.out.println("Successful...");
 			importer.removeRandomNode(x, NodeLabel.SERVER);
 			tx.success();
+		}
+	}
+	
+	public static void calc() {
+		double pAddServer = 0.15;
+		double pRemoveServer = 1 - pAddServer;
+		
+		Random r = new Random();
+		double val = r.nextGaussian(); //var = 2, x=10
+		int millisDelay = (int) Math.round(val);
+		NormalDistribution d = new NormalDistribution(10, 1);
+		
+		for(int i = 0; i < 100; i++) {
+			if(Math.random()*pAddServer > 2) {
+				
+			}
 		}
 	}
 }
