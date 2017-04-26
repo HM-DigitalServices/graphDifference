38ba717c8ee45cdfd6b599b955e2a1c2c1fcec5a
Christian Möstl <Christian Mstl>
2017-04-26 17:02:32 +0200

    modified Nodelabel

--- src/NodeLabel.java
+++ src/NodeLabel.java
@@ -1,5 +1,21 @@
 import org.neo4j.graphdb.Label;
 
 public enum NodeLabel implements Label {
-	SERVER, VM, RAM, OS, MANUFACTURER, HARDDISK, SOFTWARE, CPU, LICENCE, SERVICE;
+	SERVER(0), VM(1), RAM(2), OS(3), MANUFACTURER(4), HARDDISK(5), SOFTWARE(6), CPU(7), LICENCE(8), SERVICE(9);
+	
+    private int value;
+    
+    private NodeLabel(int value) {
+        this.value = value;
+    }
+   
+    public int getValue() {
+        return value;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(NodeLabel.RAM.getValue());
+		NodeLabel[] ar = NodeLabel.values();
+		System.out.println(ar[1] + "...");
+	}
 }
