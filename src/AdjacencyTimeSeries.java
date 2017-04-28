05b45d201c319966b13ebe8b8b7583832623c14b
Floble <baer.florian@googlemail.com>
2017-04-28 14:57:44 +0200

    added two new classes for graph prediction

--- src/AdjacencyTimeSeries.java
+++ src/AdjacencyTimeSeries.java
@@ -3,4 +3,10 @@ import java.util.Map;
 
 public class AdjacencyTimeSeries {
 	private Map<Integer, boolean[][]> timeSeries = new HashMap<Integer, boolean[][]>();
+	private Difference graphDifference;
+	
+	public void AdjacencyTimeSeries(Difference graphDifference, Map<Integer, boolean[][]> timeSeries) {
+		this.graphDifference = graphDifference;
+		this.timeSeries = timeSeries;
+	}
 }
--- src/HybridGraphAnomalyPrediction.java
+++ src/HybridGraphAnomalyPrediction.java
@@ -1,3 +1,48 @@
-public class HybridGraphAnomalyPrediction {
+import org.apache.commons.math3.stat.regression.SimpleRegression;
 
+public class HybridGraphAnomalyPrediction {
+	private SimpleRegression simpleRegression;
+	private double intercept, slope;
+	private double rsquare, significance;
+	
+	public void HybridGraphAnomalyPrediction() {
+		this.simpleRegression = new SimpleRegression();
+		this.intercept = 0.0;
+		this.slope = 0.0;
+		this.rsquare = 0.0;
+	}
+	
+	public void addTimeSeries(double[][] timeSeries) {
+		this.simpleRegression.addData(timeSeries);
+	}
+	
+	public void DetermineRegressionFunction() {
+		this.intercept = this.simpleRegression.getIntercept();
+		this.slope = this.simpleRegression.getSlope();
+	}
+	
+	public void DetermineQualityOfRegression() {
+		this.rsquare = this.simpleRegression.getRSquare();
+		this.significance = this.simpleRegression.getSignificance();
+	}
+	
+	public double predict(double timeStamp) {
+		return this.simpleRegression.predict(timeStamp);
+	}
+	
+	public double getIntercept() {
+		return this.intercept;
+	}
+	
+	public double getSlope() {
+		return this.slope;
+	}
+	
+	public double getRSquare() {
+		return this.rsquare;
+	}
+	
+	public double getSignificance() {
+		return this.significance;
+	}
 }
